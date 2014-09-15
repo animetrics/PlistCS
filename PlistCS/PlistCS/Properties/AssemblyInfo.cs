70f392e36bb956cced436ce95227a8a5d01553fd
Brian Lachniet <blachniet@gmail.com>
2014-09-15 16:26:19 -0400

    added nuspec to generate NuGet packages (nuget pack -Properties Configuration=Release)

--- PlistCS/PlistCS/Properties/AssemblyInfo.cs
+++ PlistCS/PlistCS/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PlistCS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# Property List (plist) serialization library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Animetrics")]
 [assembly: AssemblyProduct("PlistCS")]
